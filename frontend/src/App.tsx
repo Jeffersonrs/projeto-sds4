1fc724045b790abb7d86a43f334b7a875e824eb9
Jefferson Rodrigues <jeffersonrs@hotmail.com>
2021-09-08 15:22:56 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div>
-      <div>
-        <h1 className="text-primary">Olá Mundo!</h1></div>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
 export default App;
+
+
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/jeffersonrs" target="_blank" rel="noreferrer">Jefferson Rodrigues</a></p>
+                <p className="text-light"><small><strong>Projeto - Semana Spring React</strong><br />
+                    Instagram: <a href="https://www.instagram.com/jeffersonrs7/" target="_blank" rel="noreferrer">@jeffersonrs7</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
